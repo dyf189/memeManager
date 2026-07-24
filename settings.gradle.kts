1240e7b4310a20da0f0347dfa164d50f2d79424b
why37281 <why37281@126.com>
2026-07-24 19:11:43 +0800

    加阿里云 Maven 镜像（优先加速）+ reorderable 恢复 0.9.6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
